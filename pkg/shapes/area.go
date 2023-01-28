1ae8a52ac68e202b3e4873a86da1d6294906ff8e
aeff60 <ssbunklang@gmail.com>
2023-01-28 14:20:21 +0700

    create package

---
+++ pkg/shapes/area.go
@@ -0,0 +1,12 @@
+package shapes
+
+func GetRectangleArea(width, height float32) float32 {
+	return width * height
+
+}
+
+func GetCircleArea(radius float32) float32
+{
+	return math.Pi * radius * radius
+
+}
